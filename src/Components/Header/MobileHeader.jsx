58f323444eb13e3aa2e9baa781d95aedfdac44ac
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-05-27 17:58:56 +0100

    new change

---
+++ src/Components/Header/MobileHeader.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import  { HiOutlineBars3, HiXMark } from 'react-icons/hi';
+
+
+
+
+export const MobileHeader = () => {
+    return(
+        <div>
+            
+        </div>
+    )
+}
