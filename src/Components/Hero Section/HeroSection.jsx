64804da4fb948893bb57ad855398a147e007f45c
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-05-28 11:50:10 +0100

    new change

--- src/Components/Hero Section/HeroSection.jsx
+++ src/Components/Hero Section/HeroSection.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import heroImg from '../../assets/images/heroImage/heroimg2.jpg';
+import heroImg from '../../assets/images/heroImage/heroImg2.jpg';
 import { NavLink } from "react-router-dom";
 
 
