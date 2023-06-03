6e4d690a5d5e7cb9ac713a7265bc542b277ef0a9
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-06-03 13:28:13 +0100

    new change

---
+++ src/Components/User dashboard/SideLink.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import {FcAbout} from 'react-icons/fc';
+import {AiFillMessage, AiFillDashboard} from 'react-icons/ai';
+import {FaSearch} from 'react-icons/fa';
+import {MdBedroomParent} from 'react-icons/md';
+import {BsFillHouseAddFill} from 'react-icons/bs';
+import {CgProfile} from 'react-icons/cg';
+import {GrNext, GrPrevious} from 'react-icons/gr';
+
+
+export const SideLink = () => {
+
+    const [sideLinks, setSideLinks] = useState({
+        nextIc: true,
+        prevIc: false
+    })
+     const [sideLinkState, setSideLinkState] = useState('hidden');
+    return(
+        <div className="fixed left-0 top-0 bottom-0   px-[10px]  md:px-[40px] h-full md:px-[70px] bg-slate-900">
+            <div>
+            <h1 className={`flex font-bold justify-center shadow-2xl absolute w-full left-0 text-center right-0 p-2  items-center text-[20px] text-white uppercase  `}><span className={`text-[20px] md:block ${sideLinkState} font-bold text-white   `}>Homebuilder</span>   <span className="flex ml-[10px] items-center "> {sideLinks.prevIc && <GrPrevious onClick={() => {
+                setSideLinkState('hidden');
+                setSideLinks({prevIc:false, nextIc:true })
+            }} className="bg-slate-400  p-1 rounded text-[20px]"/>} <GrNext onClick={() => {
+                setSideLinkState('block');
+                setSideLinks({nextIc:false, prevIc:true,  })
+            }} className="bg-slate-400  p-1 rounded text-[20px]"/></span></h1>
+                <ul className="flex flex-col pt-[100px] gap-[40px]">
+                    <li ><Link className="flex-row   flex items-center gap-x-2  "> <AiFillDashboard className="text-slate-100 text-[30px] " />  <span className={`text-[20px] md:block ${sideLinkState} font-bold text-white   `}>Dashboard</span> </Link></li>
+                    <li><Link className="flex-row flex items-center gap-x-2  "><BsFillHouseAddFill className="text-slate-100 text-[30px] "/><span className={`text-[20px] md:block ${sideLinkState} text-white   `}>Buy</span> </Link></li>
+                    <li><Link className="flex-row flex items-center gap-x-2  "><MdBedroomParent className="text-slate-100 text-[30px] "/><span className={`text-[20px] md:block ${sideLinkState} text-white   `}>Rent</span>  </Link></li>
+                    <li><Link className="flex-row flex items-center gap-x-2  "><FaSearch className="text-slate-100 text-[30px] "/><span className={`text-[20px] md:block ${sideLinkState} text-white   `}>Search</span> </Link></li>
+                    <li><Link className="flex-row flex items-center gap-x-2  "><AiFillMessage className="text-slate-100 text-[30px] "/><span className={`text-[20px] md:block ${sideLinkState} text-white   `}>Contact</span> </Link></li>
+                    <li><Link className="flex-row flex items-center gap-x-2  "> <FcAbout className="text-slate-100 text-[30px] "/><span className={`text-[20px] md:block ${sideLinkState} text-white   `}>About</span></Link></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
--- src/Components/User dashboard/Userdashboard.jsx
+++ src/Components/User dashboard/Userdashboard.jsx
@@ -2,11 +2,10 @@ import React from "react";
 import {FaSearch} from 'react-icons/fa';
 import {MdBedroomParent} from 'react-icons/md';
 import {BsFillHouseAddFill} from 'react-icons/bs';
-import {CgProfile} from 'react-icons/cg';
+import {FaUserCircle} from 'react-icons/fa';
 import { useContext } from "react";
 import { HouseContext } from "../Context/HouseContext";
-
-
+import { SideLink } from "./SideLink";
 
 export const Userdashboard = () => {
 
@@ -14,21 +13,24 @@ export const Userdashboard = () => {
     return(
       !newUser? navig('/login') :
         <div className="absolute  items-center top-0 w-full left-0 right-0 h-full bg-white z-[20]">
-             <div className="flex w-full  flex-row items-center gap-5 justify-between p-5 bg-Tp">
-         <h1 className="flex font-bold items-center text-[20px] text-white uppercase  ">Homebuilder <BsFillHouseAddFill/></h1>
-       <button onClick={signout} className='bg-red-700 shadow-2xl text-slate-50 uppercase py-1 rounded shadow-2xl text-[20px] px-[20px] '>Logout</button>
+       <SideLink/>
+             <div className="flex w-full  md:px-[70px]  flex-row items-center gap-5 justify-between p-5 bg-white">
+         <h1 className="flex font-bold ml-[30px] items-center text-[20px] text-white uppercase  ">Homebuilder <BsFillHouseAddFill/></h1>
+         <FaUserCircle className="text-[50px] shadow-2xl rounded-full"/>
+         {/*
+       <button onClick={signout} className='bg-red-700 shadow-2xl text-slate-50 uppercase py-1 rounded shadow-2xl text-[20px] px-[20px] '></button>*/}
          </div>
-        <div className="py-[100px] flex items-center items-center justify-center  ">
+        <div className=" md:ml-[200px] flex items-end  justify-center  ">
          
           <div className="flex flex-col items-center justify-center  gap-[40px] ">
-         
-          <p className="font-[500] text-center text-[20px] md:text-[30px] ">Welcome {newUser?.email}!</p>
-        <div className="grid gap-[20px] px-[40px]  md:grid-cols-3">
-         <div className="flex justify-center min-w-[200px] min-h-[150px] p-5 flex-col items-center gap-3 shadow-2xl rounded  ">
+          <p className="font-[400] capitalize text-center text-[20px] md:text-[30px] ">Hi <span className="font-bold">{newUser?.email}</span> ,  Welcome !</p>
+          
+        <div className="grid gap-[20px] px-[40px] py-[100px]  md:grid-cols-3">
+         <div className="flex justify-center  min-w-[200px] min-h-[150px] p-5 flex-col items-center gap-3 shadow-2xl rounded  ">
             <BsFillHouseAddFill className="text-[50px]"/>
             <p className="text-[20px] uppercase font-bold">Buy Home</p>
          </div>
-         <div className="flex justify-center min-w-[200px] min-h-[150px] p-5 flex-col items-center gap-3 shadow-2xl rounded  ">
+         <div className="flex justify-center md:min-w-[250px] min-w-[200px] min-h-[150px] p-5 flex-col items-center gap-3 shadow-2xl rounded  ">
             <MdBedroomParent className="text-[50px]"/>
             <p className="text-[20px] uppercase font-bold">Rent Home</p>
          </div>
