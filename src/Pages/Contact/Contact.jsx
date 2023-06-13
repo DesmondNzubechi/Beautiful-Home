e2fa6ebb7390592c76994b0ec4d4a36cea042346
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-06-13 01:49:06 +0100

    new change

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import img from '../../assets/images/contact.avif';
-
+import Aos from "aos";
 
 
 export const Contact = () => {
+    useEffect(() => {
+        Aos.init({duration: 2000});
+    }, []);
     return(
         <div className="py-[70px] min-h-[100vh] overflow-x-hidden grid grid-cols-1 lg:grid-cols-2 gap-[20px] items-center px-[30px] pt-[150px] ">
             <form action="" method="post" className="flex flex-col gap-[20px]">
-                    <div>
+                    <div data-aos='fade-right'>
                         <h1 className="capitalize  font-semibold text-[20px] ">Get in touch with us</h1>
                     </div>
-                    <div className="flex flex-col gap-[10px] ">
+                    <div data-aos='fade-right' className="flex flex-col gap-[10px] ">
                     <div className="flex flex-col ">
                 <label className="text-[20px] font-semibold" htmlFor="">Fullname:</label>
                 <input placeholder="Input Name" className="border-pink-500 bg-pink-500 border-[2px] border rounded text-white placeholder:text-white outline-0 px-[20px] py-[5px] " type="text" name="email" id="" />
@@ -28,7 +31,7 @@ export const Contact = () => {
                   </div>
           
                 </form>
-                <div className="  ">
+                <div data-aos='fade-left' className="  ">
                     <img src={img}  alt="" />
                 </div>
         </div>
